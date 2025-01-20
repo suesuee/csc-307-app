740adc393502944d164089e2cb041874078bb83a
Sue Sue <29suesue@gmail.com>
2025-01-20 11:37:57 -0800

    Add the routes to fetch users by name and ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,10 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
+};
+
 /* findUserByName(name) returns an array of user objects matching the name.
 You wrap that array in an object with a key users_list because the API response 
 format consistently uses users_list as the key for arrays of users. */
@@ -52,6 +56,17 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; // or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found");
+  } else {
+    // result = { users_list: result }; -> {"users_list":{"id":"zap555","name":"Dennis","job":"Bartender"}}
+    res.send(result);
   }
 });
 
