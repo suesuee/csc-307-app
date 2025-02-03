4968239088afcd5f32dadbd00e8300e5663fe71f
Sue Sue <29suesue@gmail.com>
2025-02-03 09:41:00 -0800

    Remove extra GET /users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -69,28 +69,28 @@ const findUserbyNameAndJob = (name, job) => {
 You wrap that array in an object with a key users_list because the API response 
 format consistently uses users_list as the key for arrays of users.
 http://localhost:8000/users?name=Mac */
-app.get("/users", (req, res) => {
-  //   res.send(users);
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users); // when no query params, return all users
-  }
-});
-
-// app.get("/users/:id", (req, res) => {
-//   const id = req.params["id"]; // or req.params.id
-//   let result = findUserById(id);
-//   if (result === undefined) {
-//     res.status(404).send("Resource not found");
-//   } else {
-//     // result = { users_list: result }; -> {"users_list":{"id":"zap555","name":"Dennis","job":"Bartender"}}
+// app.get("/users", (req, res) => {
+//   //   res.send(users);
+//   const name = req.query.name;
+//   if (name != undefined) {
+//     let result = findUserByName(name);
+//     result = { users_list: result };
 //     res.send(result);
+//   } else {
+//     res.send(users); // when no query params, return all users
 //   }
 // });
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; // or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found");
+  } else {
+    // result = { users_list: result }; -> {"users_list":{"id":"zap555","name":"Dennis","job":"Bartender"}}
+    res.send(result);
+  }
+});
 
 // tested with curl
 app.post("/users", (req, res) => {
